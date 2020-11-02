87730a9272a0190121e9cb1357d2f26f98cc7337
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-11-02 11:44:09 -0300

    Obtendo usuário por id

--- src/main/java/com/martins/workshopmongodb/services/UserService.java
+++ src/main/java/com/martins/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.martins.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.martins.workshopmongodb.domain.User;
 import com.martins.workshopmongodb.repository.UserRepository;
+import com.martins.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,8 @@ public class UserService {
 
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	} 
 }
